b794a0e798d253693d62bf6e594c8a858f6f7110
sanikamamidwar08 <sanikamamidwar4@gmail.com>
2025-10-11 08:55:22 +0530

    install vite

---
+++ frontend/src/main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css"; // Tailwind / App CSS
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// Optional: performance metrics
+reportWebVitals();
