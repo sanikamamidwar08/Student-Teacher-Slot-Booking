9c51992197f5b749c0d1a672d6e61a238c3c2fea
sanikamamidwar08 <sanikamamidwar4@gmail.com>
2025-10-12 20:35:57 +0530

    path change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
-import Home from "./pages/Home.jsx";
+import Home from "./pages/Home.jsx"; 
 import Login from "./pages/Login.jsx";
 import Register from "./pages/Register.jsx";
 import Logout from "./pages/Logout.jsx";
