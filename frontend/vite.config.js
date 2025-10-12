8a20655332f0b1b8da0cf9aff74b84bcfef4fd10
sanikamamidwar08 <sanikamamidwar4@gmail.com>
2025-10-12 21:14:26 +0530

    add

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,25 @@
+// frontend/vite.config.js
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "url"; 
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)), 
+    },
+  },
+
   server: {
     port: 5173, // default Vite port, change if needed
     proxy: {
       // Proxy all /api requests to Django backend
+      
       "/api": {
         target: "http://127.0.0.1:8000",
         changeOrigin: true,
